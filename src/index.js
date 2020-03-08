306e693dad2cc7b6c6813578c0979c3cdeb1c2d2
dyushkin93 <d.yushkin93@gmail.com>
2020-03-08 22:55:17 +0500

    feat: task complete, test passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  str=str.split("");
+  let opening=[];
+  let closing=[];
+  let same=[];
+  for (let i=0; i<bracketsConfig.length; i++) {
+    if (bracketsConfig[i][0]==bracketsConfig[i][1]) {
+      same.push(bracketsConfig[i][0]);
+    } else {
+      opening.push(bracketsConfig[i][0]);
+      closing.push(bracketsConfig[i][1]);
+    }
+  }
+  let stack=[];
+  for (i=0; i<str.length; i++) {
+    if (opening.includes(str[i])) {
+      stack.push(str[i]);
+    } else if (closing.includes(str[i])) {
+      if (opening.indexOf(stack[stack.length-1])==closing.indexOf(str[i])) {
+        stack.pop();
+      } else return false;
+    } else if (stack[stack.length-1]==str[i]) {
+      stack.pop();
+    } else {
+      stack.push(str[i]);
+    }
+  }
+    if (stack.length==0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
